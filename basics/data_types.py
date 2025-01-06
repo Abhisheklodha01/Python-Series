707e7ea145e8088f7cd2321fbab41f699c31ff7b
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-06 17:11:12 +0530

    data type in python

---
+++ basics/data_types.py
@@ -0,0 +1,6 @@
+# in python varaibles does not have any data type. 
+# data type is always recognise by value that is hold by variable 
+# or we can say that in python memory location has a datatype
+
+value = 10  # here datatype is recognise by value not by variable name
+name = "Abhishek" # here datatype is recognise by value
