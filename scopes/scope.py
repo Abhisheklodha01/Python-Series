5ac5642bbf15e691612860eba01d53591378e67c
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-07 11:41:14 +0530

    scopes in python

---
+++ scopes/scope.py
@@ -0,0 +1,34 @@
+username = "abhisheklodha01" # globally accessible
+
+# function test() {
+#    in other languages
+# }
+
+# in python scope space is eveluated on the basis of indentation 
+
+def greet():
+    username = "Abhishek Lodha"
+    print(username)
+
+
+greet()    
+print(username)
+
+x = 87
+def func():
+    global x # gives global reference x is not redefined  
+    x = 88
+
+func()
+print(x)    
+
+# closure is also present in python which is completly similar to js 
+
+def func2(): 
+    x = 10
+    def innerFunc():
+        return x ** 2
+    return innerFunc
+
+result = func2()
+print(result())
